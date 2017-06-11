b9728160d34c6e85b49efe85c234449fe7a21e84
Justin Chiou <chiou.kai@gmail.com>
2017-06-11 11:43:12 -0400

    Modulize email script
    - Also added button to download as a pdf, but doesn't work for now.

--- certificate.php
+++ certificate.php
@@ -5,7 +5,30 @@
 ?>
 
 <body>
-    <section id="menu-0">
+    <div class="container-fluid" style="padding:12px;">
+        <div class="col-sm-2 text-center">
+            <a href="HTTP://center4certs.org" class="navbar-logo">
+                <img src="assets/images/c4c-logo-2000x1241.png" alt="Center For Certification" title="Center For Certification" width="100px">
+            </a>
+            <br />
+            <br />
+            <button class="btn btn-primary" onClick="window.print()">Print Certificate</button>
+            <button class="btn btn-primary" onClick="window.print()">Download as a PDF</button>
+        </div>
+        <div class="col-sm-10 text-center">
+            <img src="images/C4C-<?php echo $_POST['certColor']; ?>.png">
+            <div align="center" style="position: absolute; left: 311px; top: 210px; width: 418px; height: 429px;" width="1000" text-align: center; padding:20px>
+                <h3><font face="klarissa_contourregular"><?php echo $_POST['certDate'] ?></font><br /></h3>
+                <img src="images/spacer.gif" width="415" height="106"><br>
+                <h2><font face="klarissa_contourregular"><?php echo  $_POST['certification']; ?></font><br /></h2>
+                <img src="images/spacer.gif" width="413" height="98"><br>
+                <h1><font face="klarissa_contourregular"><?php echo  $_POST['fullName']; ?></font><br /></h1>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- <section id="menu-0">
         <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
             <div class="container">
                 <div class="mbr-table">
@@ -35,34 +58,25 @@
     			<img src="images/spacer.gif" width="413" height="98"><br>
     			<h1><font face="klarissa_contourregular"><?php echo  $_POST['fullName']; ?></font><br /></h1>
             </div>
-    </section>
+    </section> -->
+<style>
+    .btn + .btn {
+        margin-left: 0;
+    }
+</style>
+
+<!-- <footer>
+    <div class="container">
+        <p class="text-xs-center">&copy;
+            <?php echo date("Y"); ?>
+            <a href="mailto:Dr-Cert@center4certs.org">Center For Certification</a>
+        </p>
+    </div>
+</footer> -->
 
 <?php
 
-require 'includes/PHPMailer-master/PHPMailerAutoload.php';
-
-$mail = new PHPMailer;
-$mail->setFrom('dr-cert@center4certs.org', 'Dr. Cert');         // How you wanna see it in your inbox
-$mail->addAddress('dr-cert@center4certs.org', 'Dr. Cert');      // Whom it's being sent to
-// $mail->addAddress('dr-cert@center4certs.org');               // Name is also optional
-// $mail->addReplyTo('info@example.com', 'Information');        // In case you wanna send to client
-// $mail->addAttachment('/var/tmp/file.tar.gz');                // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');           // Optional name
-$mail->isHTML(true);                                            // Set email format to HTML
-$mail->Subject = $_POST['fullName'].' Certified Themself!';
-$mail->Body    = $_POST['fullName'].' is now'."<br />";
-$mail->Body    .= $_POST['certification'].' as of '."<br />";
-$mail->Body    .= $_POST['certDate'].' and received a '."<br />";
-$mail->Body    .= $_POST['certColor'].' certificate.'."<br /><br />";
-$mail->Body    .= $_POST['email'].' '."<br />";
-
-if(!$mail->send()) {
-    echo 'Page could not be sent.<br />';
-    echo 'Page Error: ' . $mail->ErrorInfo;                   // Shows at the bottom of the page
-} else {
-    echo '<br />You are officially certified by the powers vested in the Center for Certification.';                               // Shows at the bottom of the page
-}
-
-include 'templates/footer.php';
+include 'includes/emailAdmin.php';
+// include 'templates/footer.php';
 
 ?>
---
+++ includes/emailAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'includes/PHPMailer-master/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+$mail->setFrom('dr-cert@center4certs.org', 'Dr. Cert');         // How you wanna see it in your inbox
+$mail->addAddress('dr-cert@center4certs.org', 'Dr. Cert');      // Whom it's being sent to
+// $mail->addAddress('dr-cert@center4certs.org');               // Name is also optional
+// $mail->addReplyTo('info@example.com', 'Information');        // In case you wanna send to client
+// $mail->addAttachment('/var/tmp/file.tar.gz');                // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');           // Optional name
+$mail->isHTML(true);                                            // Set email format to HTML
+$mail->Subject = $_POST['fullName'].' Certified Themself!';
+$mail->Body    = $_POST['fullName'].' is now'."<br />";
+$mail->Body    .= $_POST['certification'].' as of '."<br />";
+$mail->Body    .= $_POST['certDate'].' and received a '."<br />";
+$mail->Body    .= $_POST['certColor'].' certificate.'."<br /><br />";
+$mail->Body    .= $_POST['email'].' '."<br />";
+
+if(!$mail->send()) {
+    echo 'Page could not be sent.<br />';
+    echo 'Page Error: ' . $mail->ErrorInfo;                   // Shows at the bottom of the page
+} else {
+    echo '<br />You are officially certified by the powers vested in the Center for Certification.';                               // Shows at the bottom of the page
+}
